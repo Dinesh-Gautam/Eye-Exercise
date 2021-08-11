dc05ddf0cb165ef0ef2339272eac81042bd470eb
Dinesh-Gautam <dineshgautam5252@gmail.com>
2021-08-12 03:07:33 +0530

    fixing commits

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,37 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: ["./src/scripts/thirdParty/gsap.min.js", "./src/scripts/main.js"],
+  output: {
+    filename: "main.bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
   module: {
     rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true },
+          },
+        ],
+      },
+
       {
         test: /\.mp3$/,
         loader: "file-loader",
       },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
     ],
   },
-
-  output: {
-    filename: "main.bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
+  optimization: {
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "index.html",
     }),
-    new MiniCssExtractPlugin({
-      linkType: "text/css",
-    }),
   ],
-  optimization: {
-    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
-  },
 };
